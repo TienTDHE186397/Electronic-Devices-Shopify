1e92a170c59bb1c6d7f8e12945b47643b484a77d
Thanh Trung <trungnthe173045@fpt.edu.vn>
2024-09-19 22:04:25 +0700

    Reup Entity

--- src/java/Entity/Person.java
+++ src/java/Entity/Person.java
@@ -1,11 +1,10 @@
 package Entity;
 
-<<<<<<< HEAD
 import java.util.*;
 import java.lang.*;
 
 public class Person {
-
+    
     private int person;
     private String name;
     private String gender;
@@ -13,30 +12,13 @@ public class Person {
     private String address;
     private String email;
     private String phone;
-    private Role RoleId;
+    private int RoleId;
     private String password;
-=======
-import java.sql.Date;
-
-public class Person {
-
-    private int PersonID;
-    private String Name;
-    private String Gender;
-    private String DateOfBirth;
-    private String StartDate;
-    private String Address;
-    private String Email;
-    private String Phone;
-    private int roleID;
-    private String Pasword;
->>>>>>> 73bfbee9d457f695ae4defefabbc3ceffb151c11
 
     public Person() {
     }
 
-<<<<<<< HEAD
-    public Person(int person, String name, String gender, Date DateOfBirth, String address, String email, String phone, Role RoleId, String password) {
+    public Person(int person, String name, String gender, Date DateOfBirth, String address, String email, String phone, int RoleId, String password) {
         this.person = person;
         this.name = name;
         this.gender = gender;
@@ -50,104 +32,33 @@ public class Person {
 
     public int getPerson() {
         return person;
-=======
-    public Person(int PersonID, String Name, String Gender, String DateOfBirth, String Address, String Email, String Phone, int roleID, String Pasword, String code) {
-        this.PersonID = PersonID;
-        this.Name = Name;
-        this.Gender = Gender;
-        this.DateOfBirth = DateOfBirth;
-        this.Address = Address;
-        this.Email = Email;
-        this.Phone = Phone;
-        this.roleID = roleID;
-        this.Pasword = Pasword;
-
-    }
-
-    public Person(String Name, String Gender, String DateOfBirth, String StartDate, String Address, String Email, String Phone, int roleID, String Pasword) {
-        this.Name = Name;
-        this.Gender = Gender;
-        this.DateOfBirth = DateOfBirth;
-        this.StartDate = StartDate;
-        this.Address = Address;
-        this.Email = Email;
-        this.Phone = Phone;
-        this.roleID = roleID;
-        this.Pasword = Pasword;
-
-    }
-
-    public int getPersonID() {
-        return PersonID;
-    }
-
-    public void setPersonID(int PersonID) {
-        this.PersonID = PersonID;
->>>>>>> 73bfbee9d457f695ae4defefabbc3ceffb151c11
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
-<<<<<<< HEAD
-=======
-    public void setName(String Name) {
-        this.Name = Name;
-    }
-
->>>>>>> 73bfbee9d457f695ae4defefabbc3ceffb151c11
     public String getGender() {
-        return Gender;
+        return gender;
     }
 
-<<<<<<< HEAD
     public Date getDateOfBirth() {
         return DateOfBirth;
     }
 
     public String getAddress() {
         return address;
-=======
-    public void setGender(String Gender) {
-        this.Gender = Gender;
-    }
-
-    public String getDateOfBirth() {
-        return DateOfBirth;
-    }
-
-    public void setDateOfBirth(String DateOfBirth) {
-        this.DateOfBirth = DateOfBirth;
-    }
-
-    public String getStartDate() {
-        return StartDate;
-    }
-
-    public void setStartDate(String StartDate) {
-        this.StartDate = StartDate;
-    }
-
-    public String getAddress() {
-        return Address;
-    }
-
-    public void setAddress(String Address) {
-        this.Address = Address;
->>>>>>> 73bfbee9d457f695ae4defefabbc3ceffb151c11
     }
 
     public String getEmail() {
-        return Email;
+        return email;
     }
 
-<<<<<<< HEAD
     public String getPhone() {
         return phone;
     }
 
-    public Role getRoleId() {
+    public int getRoleId() {
         return RoleId;
     }
 
@@ -181,47 +92,17 @@ public class Person {
 
     public void setPhone(String phone) {
         this.phone = phone;
-=======
-    public void setEmail(String Email) {
-        this.Email = Email;
     }
 
-    public String getPhone() {
-        return Phone;
-    }
-
-    public void setPhone(String Phone) {
-        this.Phone = Phone;
-    }
-
-    public int getRoleID() {
-        return roleID;
->>>>>>> 73bfbee9d457f695ae4defefabbc3ceffb151c11
-    }
-
-    public void setRoleId(Role RoleId) {
+    public void setRoleId(int RoleId) {
         this.RoleId = RoleId;
     }
 
-<<<<<<< HEAD
     public void setPassword(String password) {
         this.password = password;
     }
     
     
-=======
-    public String getPasword() {
-        return Pasword;
-    }
 
-    public void setPasword(String Pasword) {
-        this.Pasword = Pasword;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" + "PersonID=" + PersonID + ", Name=" + Name + ", Gender=" + Gender + ", DateOfBirth=" + DateOfBirth + ", StartDate=" + StartDate + ", Address=" + Address + ", Email=" + Email + ", Phone=" + Phone + ", roleID=" + roleID + ", Pasword=" + Pasword + '}';
-    }
->>>>>>> 73bfbee9d457f695ae4defefabbc3ceffb151c11
 
 }
--- src/java/Entity/Product.java
+++ src/java/Entity/Product.java
@@ -18,20 +18,48 @@ public class Product {
     private int views;
     private Date releaseDate;
     private int quantitySold;
-    private int categoryID;
+    private Categories categoryID;
     private int quantity;
     private int sale;
 
     public Product() {
     }
 
-    public Product(int productID, int views, Date releaseDate, int quantitySold, int categoryID, int quantity, int sale) {
+    public Product(int productID, int views, Date releaseDate, int quantitySold, Categories categoryID, int quantity, int sale) {
         this.productID = productID;
         this.views = views;
         this.releaseDate = releaseDate;
         this.quantitySold = quantitySold;
         this.categoryID = categoryID;
         this.quantity = quantity;
+        this.sale = sale;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setQuantitySold(int quantitySold) {
+        this.quantitySold = quantitySold;
+    }
+
+    public void setCategoryID(Categories categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setSale(int sale) {
         this.sale = sale;
     }
 
@@ -51,7 +79,7 @@ public class Product {
         return quantitySold;
     }
 
-    public int getCategoryID() {
+    public Categories getCategoryID() {
         return categoryID;
     }
 
@@ -63,33 +91,6 @@ public class Product {
         return sale;
     }
 
-    public void setProductID(int productID) {
-        this.productID = productID;
-    }
-
-    public void setViews(int views) {
-        this.views = views;
-    }
-
-    public void setReleaseDate(Date releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public void setQuantitySold(int quantitySold) {
-        this.quantitySold = quantitySold;
-    }
-
-    public void setCategoryID(int categoryID) {
-        this.categoryID = categoryID;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setSale(int sale) {
-        this.sale = sale;
-    }
     
     
     
