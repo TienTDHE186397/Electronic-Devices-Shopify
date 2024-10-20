a31266747d9863cd8ae125052d207ed87da50745
Nghiem Minh Duc (HE186285) <ducnmhe186285@fpt.edu.vn>
2024-10-21 00:20:05 +0700

    Update adminDashBoard

--- src/java/Controller/adminProfile.java
+++ src/java/Controller/adminProfile.java
@@ -4,9 +4,8 @@
  */
 package Controller;
 
-import DAO.DAOAdmin;
-import Entity.Person;
 import DAO.DAOPerson;
+import Entity.Person;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -14,14 +13,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.List;
 
 /**
  *
  * @author nghie
  */
-@WebServlet(name = "UserListServlet", urlPatterns = {"/userList"})
-public class UserListServlet extends HttpServlet {
+@WebServlet(name = "adminProfile", urlPatterns = {"/adminProfile"})
+public class adminProfile extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -35,18 +33,7 @@ public class UserListServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UserListServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UserListServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -61,14 +48,12 @@ public class UserListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String id = request.getParameter("PersonID");//sai
+        
         DAOPerson dp = new DAOPerson();
-        DAOAdmin da = new DAOAdmin();
-        String id = request.getParameter("PersonID");
-        Person p = da.getPersonById(id);
-        request.setAttribute("person", p);
-        List<Person> listP = dp.getAllPerson();
-        request.setAttribute("listP", listP);
-        request.getRequestDispatcher("UserList.jsp").forward(request, response);
+            Person p = dp.getPersonById(id);
+        request.setAttribute("detail", p);
+        request.getRequestDispatcher("AdminProfile.jsp").forward(request, response);
     }
 
     /**
@@ -82,7 +67,11 @@ public class UserListServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        String id = request.getParameter("PersonID");//sai
+        DAOPerson dp = new DAOPerson();
+            Person p = dp.getPersonById(id);
+        request.setAttribute("detail", p);
+        request.getRequestDispatcher("AdminProfile.jsp").forward(request, response);
     }
 
     /**
