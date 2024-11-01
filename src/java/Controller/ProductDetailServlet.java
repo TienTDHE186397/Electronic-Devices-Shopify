bfd5cb3b3084318f5dcbea23ece1eaa1d4efbfff
VuDucHai_HE181844 <haivdhe181844@fpt.edu.vn>
2024-11-01 12:45:41 +0700

    Sync

--- src/java/Controller/ProductDetailServlet.java
+++ src/java/Controller/ProductDetailServlet.java
@@ -51,7 +51,7 @@ public class ProductDetailServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            throws ServletException, IOException {
+             {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
